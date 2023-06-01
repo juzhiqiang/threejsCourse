0f029ae87cd6767a39e07bbe8d9e5120a252fa17
juzhiqinag <1020814597@qq.com>
2023-06-01 10:52:45 +0800

    新增房间查看功能

--- threeDm/src/pages/dataHouse/index.tsx
+++ threeDm/src/pages/dataHouse/index.tsx
@@ -2,7 +2,7 @@
  * @Author: juzhiqinag 1020814597@qq.com
  * @Date: 2023-05-29 22:46:09
  * @LastEditors: juzhiqinag 1020814597@qq.com
- * @LastEditTime: 2023-05-31 01:15:24
+ * @LastEditTime: 2023-05-31 22:52:59
  * @FilePath: \threejsCourse\threeDm\src\pages\dataHouse\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,10 +17,7 @@
 import styles from "./index.less";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import { Room } from "./Room";
-import { SpriteText } from "./SpriteText";
 import { gsap } from "gsap";
-import { MouseLinster } from "./mouseLinster";
 // @ts-ignore
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import RoomShapeMesh from "./threeMesh/RoomShapeMesh";
@@ -29,6 +26,11 @@ import Wall from "./threeMesh/Wall";
 
 const ViewHouse = () => {
   const viewHome = useRef<HTMLDivElement | null>(null);
+  const roomData = useRef<any>({
+    roomIndex: 0,
+    timeline: gsap.timeline(),
+    dir: new THREE.Vector3(),
+  });
   const [progress, setProgress] = useState("0");
   const [init, setInit] = useState(0);
   useEffect(() => {
@@ -43,6 +45,7 @@ const ViewHouse = () => {
       0.1,
       1000
     );
+    console.log(camera)
     // 设置相机初始w位置
     camera.position.set(0, 2, 5.5);
     // 初始化渲染器
@@ -67,9 +70,6 @@ const ViewHouse = () => {
 
     viewHome.current.appendChild(renderer.domElement);
 
-    // 鼠标移动监控
-    new MouseLinster(viewHome.current, camera);
-
     // 加载全景图
     const loader = new THREE.TextureLoader();
     const texture = loader.load("/dataHouse/sky.jpg");
@@ -78,11 +78,15 @@ const ViewHouse = () => {
     scene.background = texture;
     scene.environment = texture;
 
+    roomData.current.camera = camera;
+    roomData.current.scene = scene;
+    roomData.current.controls = controls;
     // 获取接口数据
     let roomIdToPanorama: any = {};
     fetch("/api/three0313/demo720.json")
       .then((res) => res.json())
       .then((obj) => {
+        roomData.current.panoramaLocation = obj.panoramaLocation;
         // 循环创建房间
         for (let i = 0; i < obj.objData.roomList.length; i++) {
           // 获取房间数据
@@ -133,9 +137,52 @@ const ViewHouse = () => {
     // };
   }, [viewHome.current]);
 
+  const changeRoom = () => {
+    let room = roomData.current.panoramaLocation[roomData.current.roomIndex];
+    roomData.current.dir = roomData.current.camera.position
+      .clone()
+      .sub(
+        new THREE.Vector3(
+          room.point[0].x / 100,
+          room.point[0].z / 100,
+          room.point[0].y / 100
+        )
+      )
+      .normalize();
+
+    roomData.current.timeline.to(roomData.current.camera.position, {
+      duration: 1,
+      x: room.point[0].x / 100 + roomData.current.dir.x * 0.01,
+      y: room.point[0].z / 100,
+      z: room.point[0].y / 100 + roomData.current.dir.z * 0.01,
+    });
+    // 改变相机关注点
+    console.log(roomData.current.camera)
+    // roomData.current.camera.looAt(
+    //   room.point[0].x / 100,
+    //   room.point[0].z / 100,
+    //   room.point[0].y / 100
+    // );
+
+    roomData.current.controls.target.set(
+      room.point[0].x / 100,
+      room.point[0].z / 100,
+      room.point[0].y / 100
+    );
+    roomData.current.roomIndex = roomData.current.roomIndex + 1;
+    if (
+      roomData.current.roomIndex >= roomData.current.panoramaLocation.length
+    ) {
+      roomData.current.roomIndex = 0;
+    }
+  };
+
   return (
     <>
       <div className={styles.main} ref={viewHome}></div>
+      <div className={styles.btn} onClick={() => changeRoom()}>
+        切换房间
+      </div>
       {/* <div
         style={{
           position: "fixed",
